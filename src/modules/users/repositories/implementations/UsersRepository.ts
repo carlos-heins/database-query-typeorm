4d54a491b9d4dd2c9dd3e3acee46867f2c79b381
carlos-heins <Carlozheins053@gmail.com>
2022-01-25 02:04:16 -0300

    projeto finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,23 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {
+      relations: ["games"]
+    })
+
+    return user as User; // foi uma forma que eu encontrei de contornar o erro.
+    // outra forma que achei foi usar "non-null assertion" (user!), mas li que é errado.
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name ASC`);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE Lower(first_name) = Lower($1) AND Lower(last_name) = Lower($2)`, 
+    [first_name, last_name]);
   }
 }
